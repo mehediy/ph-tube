82b137bd0d9e830c19f9e4ff435fcd17918bddb0
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-09-01 21:09:09 +0600

    display card data

--- script.js
+++ script.js
@@ -27,8 +27,36 @@ const loadData = async (id) => {
     `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
   const data = await res.json();
-  console.log(data);
+  //   console.log(data);
+  displayCards(data.data);
 };
+loadData(1000);
 
 // Display Cards
 const cardContainer = document.getElementById("cards");
+const displayCards = (data) => {
+  data.forEach((data) => {
+    console.log(data);
+
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <!-- Card -->
+    <div>
+      <div class="rounded mb-4 overflow-hidden h-[200px] bg-[url('${data.thumbnail}')] bg-cover bg-no-repeat bg-center"></div>
+      <!-- Card info -->
+      <div class="flex gap-2">
+        <div class="self-start rounded-full overflow-hidden h-[40px] w-[40px]">
+          <img class="h-[40px] w-[40px]" src="${data.authors[0].profile_picture}" alt="" />
+        </div>
+        <div class="flex flex-col gap-2">
+          <a href="#" class="font-bold">${data.title}</a>
+          <h3 class="text-sm text-gray-500">${data.authors[0].profile_name} <span></span></h3>
+          <span class="text-sm text-gray-500">${data.others.views}</span>
+        </div>
+      </div>
+    </div>
+    `;
+
+    cardContainer.append(card);
+  });
+};
